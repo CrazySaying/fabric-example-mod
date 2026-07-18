6a3e513822b614b6b88e781ea509a2e228c5c2af
CrazySaying <1445343715@qq.com>
2026-07-18 16:43:26 +0800

    基于Originfolia的仅服务端环境调整的模组模板

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,6 +1,8 @@
 package com.example.mixin;
 
 import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,8 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
+	private static final Logger LOGGER = LoggerFactory.getLogger("example-mixin");
+
+	@Inject(at = @At("HEAD"), method = "tickServer")
+	private void onTickServer(CallbackInfo info) {
+		LOGGER.info("Mixin injected into MinecraftServer.tickServer()!");
 	}
 }
